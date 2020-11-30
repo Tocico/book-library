b259b401f5cd2b1a07d5e47823983f5e7045ede2
Toshiko <kunotoshiko17@gmail.com>
2020-11-30 15:53:57 +0100

    Added classes in model

---
+++ src/main/java/Model/Language.java
@@ -0,0 +1,12 @@
+package Model;  /**
+   * Created by Toshiko Kuno
+   * Date: 2020-11-30
+   * Time: 15:53
+   * Project: IntelliJ IDEA
+   * Copyright: MIT
+   */ 
+    
+    
+    
+    public enum Language {
+}
---
+++ src/main/java/Model/Library.java
@@ -0,0 +1,12 @@
+package Model;  /**
+   * Created by Toshiko Kuno
+   * Date: 2020-11-30
+   * Time: 15:52
+   * Project: IntelliJ IDEA
+   * Copyright: MIT
+   */ 
+    
+    
+    
+    public class Library {
+}
