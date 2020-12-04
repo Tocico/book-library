556ec6108280cdedcce677f95f8b6e15d0d49d3f
Max J <14187946+m4xiJo@users.noreply.github.com>
2020-12-04 03:05:01 +0100

    Update StorageUtil.java

--- src/main/java/Model/StorageUtil.java
+++ src/main/java/Model/StorageUtil.java
@@ -4,20 +4,31 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class StorageUtil {
 
+public class StorageUtil {
     public StorageUtil(String path) throws IOException {
         FileWriter dbWrite = new FileWriter(path);
         FileReader dbRead = new FileReader(path);
     }
 
-    public String selectWhere(String key) {
+    public String selectFrom(Integer index, String where) {
         return "";
         //new JSONArray();
-    }
-
-    public void insertWhere(String key) {
 
     }
+
+    public void insertTo(Integer index, String where) {
+
+    }
+
+    public void createEntry(Object obj) {
+
+    }
+
+    public void deleteEntry(String where) {
+
+
+    }
+
 
 }
