e162e5a6d7ec217efd8d37fd10e8235fb054b51c
miwgu <miwa.guhres@hotmail.com>
2020-12-05 11:54:34 +0100

    Add RegisterUser GUI, RegisterUser class

---
+++ src/main/java/Client/Controller/Employee/RegisterUser.java
@@ -0,0 +1,15 @@
+package Client.Controller.Employee;
+
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * Created by Miwa Guhrés
+ * Date: 2020-12-05
+ * Time: 10:18
+ * Project: Bibliotek
+ * Copyright: MIT
+ */
+public class RegisterUser {
+    public AnchorPane RegisterUser;
+
+}
