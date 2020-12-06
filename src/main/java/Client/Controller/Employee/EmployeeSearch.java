acd39feda2dc5929958f806da77b1bbc1d40ae4d
miwgu <miwa.guhres@hotmail.com>
2020-12-06 11:18:43 +0100

    Add EmployeeSearch employeeSearch GUI

---
+++ src/main/java/Client/Controller/Employee/EmployeeSearch.java
@@ -0,0 +1,11 @@
+package Client.Controller.Employee;
+
+/**
+ * Created by Miwa Guhrés
+ * Date: 2020-12-05
+ * Time: 23:58
+ * Project: Bibliotek
+ * Copyright: MIT
+ */
+public class EmployeeSearch {
+}
