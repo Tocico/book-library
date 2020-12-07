2b1c061ef99e823404d023cb3342ab0b3fe555e3
miwgu <miwa.guhres@hotmail.com>
2020-12-07 21:45:28 +0100

    Add searchBookManage, modalBookManage GUI Add BookManage, searchBookManage class

---
+++ src/main/java/Client/Controller/Employee/BookManage.java
@@ -0,0 +1,11 @@
+package Client.Controller.Employee;
+
+/**
+ * Created by Miwa Guhrés
+ * Date: 2020-12-07
+ * Time: 14:42
+ * Project: Bibliotek
+ * Copyright: MIT
+ */
+public class BookManage {
+}
