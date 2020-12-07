5f2917ad4c1661f9bd850c0c55f1bbe48f84e6a3
Max J <14187946+m4xiJo@users.noreply.github.com>
2020-12-07 16:21:59 +0100

    Super Toshiko

--- src/main/java/DAO/BookDao.java
+++ src/main/java/DAO/BookDao.java
@@ -3,6 +3,8 @@ package DAO;
 import Model.Book;
 import Model.Category;
 import Model.Language;
+import Model.StorageUtil;
+import Model.UserEntities.User;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,6 +23,7 @@ public class BookDao implements Dao<Book> {
 
     //Spara data som hämtar från textfil i lista
     private List<Book> bookList = new ArrayList<>();
+    private StorageUtil db;
 
     //Hämta data från textfil?
     public BookDao() {
@@ -29,47 +32,68 @@ public class BookDao implements Dao<Book> {
         bookList.add(new Book().setTitle("Load of the rings").setIsbn("22343").setCategory(Category.FANTASY).setLanguage(Language.Engelska).setAuthor("J. R. R. Tolkien").setNumberOfBooks(4));
         bookList.add(new Book().setTitle("Gozilla").setIsbn("32343").setCategory(Category.SCIENCE_FICTION).setLanguage(Language.Engelska).setAuthor("Tomoyuki Tanaka").setNumberOfBooks(1));
         /*------------------------------------------*/
+
+
+        try {
+            db = new StorageUtil("books");
+            //saveAll();
+            bookList = getAll(); //Overwrite current history list with the fetched deserialized data
+
+            System.out.println("Loaded data" + bookList);
+        } catch (ClassNotFoundException | IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
     @Override
-    public List<Book> getAll() {
-        return bookList;
+    public List<Book> getAll() throws IOException, ClassNotFoundException {
+        return db.deserializeReadList();
     }
 
     @Override
     public void saveAll() throws IOException {
-
+        db.serializeStoreList(bookList);
     }
 
     //Register book
     @Override
-    public void save(Book book) {
+    public void save(Book book) throws IOException {
         bookList.add(book);
-        //TODO: Updatera db? Deserialize?
+        saveAll();
     }
 
     //Update book
     @Override
-    public void update(Book book) {
+    public void update(Book book) throws IOException {
         bookList.get(bookList.indexOf(book));
-        //TODO: Updatera db? Deserialize?
+        saveAll();
     }
 
     //Remove book
     @Override
-    public void delete(Book book) {
-       bookList.remove(bookList.indexOf(book));
-       //TODO: Updatera db? Deserialize?
+    public void delete(Book book) throws IOException {
+        bookList.remove(bookList.indexOf(book));
+        saveAll();
     }
 
     @Override
     public Book getById(String id) {
+        for (Object e : bookList) {
+            if (e instanceof Book) {
+                if(((Book) e).getId().equals(id)) return (Book) e;
+            }
+        }
         return null;
     }
 
-    @Override
-    public Book getByName(String name) {
-        return null;
+    public List<Book> getByName(String name) {
+        List<Book> foundBooks = new ArrayList<>();
+        for (Object e : bookList) {
+            if (e instanceof Book) {
+                if((((Book) e).getTitle().equals(name))) foundBooks.add((Book) e);
+            }
+        }
+        return foundBooks;
     }
 
 
--- src/main/java/DAO/UserDao.java
+++ src/main/java/DAO/UserDao.java
@@ -1,6 +1,8 @@
 package DAO;
 
 import Model.Book;
+import Model.History;
+import Model.StorageUtil;
 import Model.UserEntities.Employee;
 import Model.UserEntities.User;
 import Model.UserEntities.Visitor;
@@ -22,55 +24,77 @@ public class UserDao implements Dao<User> {
 
     //Spara data som hämtar från textfil i lista
     private List<User> userList = new ArrayList<>();
+    private StorageUtil db;
 
     //Hämta data från textfil?
     public UserDao() {
+
         /*------------------------TEST----------------------------------*/
         userList.add(new Employee("Toshiko", "Kuno", "8811072886", "Enskede", "kuno@gmail.com", "1111", "11111111", true));
         userList.add(new Employee("Miwa", "", "0000000000", "", "miwa@gmail.com", "2222","11111111",true));
         userList.add(new Visitor("Yohannes", "", "1111111111", "", "yohannes@gmail.com", "3333" ,"11111111",false));
         userList.add(new Visitor("Maxim", "", "2222222222", "", "maxim@gmail.com", "4444", "11111111",false));
+        /*------------------------------------------*/
+
+        try {
+            db = new StorageUtil("books");
+            //saveAll();
+            userList = getAll(); //Overwrite current history list with the fetched deserialized data
+            System.out.println("Loaded data" + userList);
+        } catch (ClassNotFoundException | IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
-    
 
     @Override
-    public List<User> getAll() {
-        return userList;
+    public List<User> getAll() throws IOException, ClassNotFoundException {
+        return db.deserializeReadList();
     }
 
     @Override
     public void saveAll() throws IOException {
-
+        db.serializeStoreList(userList);
     }
 
     //Register user
     @Override
-    public void save(User user) {
+    public void save(User user) throws IOException {
         userList.add(user);
-        //TODO: Updatera db? Deserialize?
+        saveAll();
     }
 
     //Update user
     @Override
-    public void update(User user) {
+    public void update(User user) throws IOException {
         userList.get(userList.indexOf(user));
-        //TODO: Updatera db? Deserialize?
+        saveAll();
     }
 
     //Remove user
     @Override
-    public void delete(User user) {
+    public void delete(User user) throws IOException {
         userList.remove(userList.indexOf(user));
-        //TODO: Updatera db? Deserialize?
+        saveAll();
     }
 
     @Override
     public User getById(String id) {
+        for (Object e : userList) {
+            if (e instanceof User) {
+                if(((User) e).getsSN().equals(id)) return (User) e;
+            }
+        }
         return null;
     }
 
-    @Override
-    public User getByName(String name) {
-        return null;
+    public List<User> getByName(String name) {
+        List<User> foundUsers = new ArrayList<>();
+        for (Object e : userList) {
+            if (e instanceof User) {
+                if((((User) e).getFirstName() + " " + ((User) e).getFirstName()).equals(name)) foundUsers.add((User) e);
+            }
+        }
+        return foundUsers;
     }
 }
